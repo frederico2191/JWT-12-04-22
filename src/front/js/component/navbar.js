4a243fcf6235a7e1ef0166e5a4a8883a4367dcb4
Frederico Rodrigues <frederico2191@gmail.com>
2023-04-19 17:29:28 +0000

    Login done, sign up waiting

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        </Link>
+        <div className="ml-auto">
+          {!store.token ? (
+            <div>
+              <Link to="/login">
+                <button className="btn btn-primary">Please Log In</button>
+              </Link>
+              <Link to="/signup">
+                <button className="btn btn-primary">Please Sign up</button>
+              </Link>
+            </div>
+          ) : (
+            <button
+              onClick={() => actions.logout()}
+              className="btn btn-primary"
+            >
+              LOG OUT
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,122 @@
+import { useNavigate } from "react-router-dom";
+
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      message: null,
+      token: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      syncTokenFromSessionStore: () => {
+        const token = sessionStorage.getItem("token");
+        console.log("I am the reloaded token", token);
+        const store = getStore();
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+        if (store.token && store.token != "" && store.token != undefined) {
+          setStore({ token: token });
+        }
+      },
+      getMessage: async () => {
+        const store = getStore();
+        const opts = {
+          headers: {
+            Authorization: "Bearer " + store.token,
+          },
+        };
+        try {
+          const resp = await fetch(
+            process.env.BACKEND_URL + "/api/hello",
+            opts
+          );
+          const data = await resp.json();
+          setStore({ message: data.message });
+          return true;
+        } catch (error) {
+          console.error(
+            "There was an error!!! It was caught by flux.js",
+            error
+          );
+        }
+      },
+      logout: () => {
+        const token = sessionStorage.removeItem("token");
+        console.log("logging out");
+        setStore({ token: null });
+      },
+      login: async (email, password) => {
+        const opts = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        };
+        try {
+          const resp = await fetch(
+            process.env.BACKEND_URL + "/api/token",
+            opts
+          );
+          if (!resp.ok) throw Error("There was a problem in the login request");
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+          if (resp.status === 401) {
+            throw "Invalid credentials";
+          } else if (resp.status === 400) {
+            throw "Invalid email or password format";
+          }
+          const data = await resp.json();
+          sessionStorage.setItem("token", data.access_token);
+          setStore({ token: data.access_token });
+          return true;
+        } catch (error) {
+          console.error(
+            "There was an error!!! It was caught by flux.js",
+            error
+          );
+        }
+      },
+    },
+  };
+  //   Login2: async (email, password) => {
+  //     try {
+  //       // fetching data from the backend
+  //       const resp = await fetch(process.env.BACKEND_URL + "/api/login", {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify({ email: email, password: password }),
+  //       });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  //       if (!resp.ok) throw Error("There was a problem in the login request");
+
+  //       if (resp.status === 401) {
+  //         throw "Invalid credentials";
+  //       } else if (resp.status === 400) {
+  //         throw "Invalid email or password format";
+  //       }
+  //       const data = await resp.json();
+
+  //       // save your token in the localStorage
+  //       //also you should set your user into the store using the setStore function
+  //       console.log("data:::    " + data);
+  //       localStorage.setItem("jwt-token", data.token);
+  //     } catch (error) {
+  //       console.log("Error login from backend", error);
+  //     }
+  //   }
 };
 
 export default getState;
